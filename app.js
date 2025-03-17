36c3330d559670e993bde8087f3f3b59dfd48e30
Maria <vasquezms@hotmail.com>
2025-03-17 20:17:19 -0300

    Subiendo desafío amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let listaAmigos = document.getElementById("listaAmigos"); 
+
+    let nombre = inputAmigo.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombre);
+    actualizarLista();
+    inputAmigo.value = "";
+}
+
+function actualizarLista() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = ""; 
+
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li); 
+    }
+}
+
+function sortearAmigo() {
+    let resultado = document.getElementById("resultado");
+
+    if (amigos.length === 0) {
+        resultado.innerHTML = "<li>No hay amigos para sortear.</li>";
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    resultado.innerHTML = `<li> Amigo secreto: <strong>${amigoSorteado}</strong> </li>`;
+}
